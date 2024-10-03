46a12095fbb7293aac13dc95222738a395910f3c
dresiarz222 <supremin222@wp.pl>
2024-10-03 15:11:49 +0200

    asd

--- utils.lua
+++ utils.lua
@@ -107,17 +107,19 @@ function d.antiAfk()
     task.spawn(function()
         while task.wait(math.random(1,6)*10) do
             
+            local randomNum = math.random(1,10)
+
             xpcall(function()
                 d.vu:CaptureController()
-                d.vu:ClickButton2(Vector2.new(math.random(0,d.screenX),math.random(0,d.screenY)))
+                d.vu:ClickButton2(Vector2.new(d.screenX/2+randomNum,d.screenY+randomNum))
             end, function(...)
                 warn("antiafk 1 fail", ...)
             end)
     
             xpcall(function()
-                d.vu:Button2Down(Vector2.new(math.random(0,d.screenX),math.random(0,d.screenY)),workspace.CurrentCamera.CFrame)
+                d.vu:Button2Down(Vector2.new(d.screenX/2+randomNum,d.screenY+randomNum),workspace.CurrentCamera.CFrame)
                 task.wait(1)
-                d.vu:Button2Up(Vector2.new(math.random(0,d.screenX),math.random(0,d.screenY)),workspace.CurrentCamera.CFrame)
+                d.vu:Button2Up(Vector2.new(d.screenX/2+randomNum,d.screenY+randomNum),workspace.CurrentCamera.CFrame)
             end, function(...)
                 warn("antiafk 2 fail", ...)
             end)
